9393cac8728a0fe07f90761b0355197849b768b1
Ilyas Shomayev <ilyasshomayev@MacBook-Air-Ilyas.local>
2017-10-06 13:23:52 -0400

    battery chart added

--- server.R
+++ server.R
@@ -8,11 +8,14 @@ efficiency = 0.16
 multiplier = 1000/(30*24)
 
 solar_data = fread('solar_data.csv')
-#consumption = fread('consumpiton.csv')
+#solar_data = solar_data %>% mutate(date = as.Date(paste('2017', month, day, sep = '/')))
+
+consumption = fread('consumption_v2.csv')
+
 
 function(input, output){
-  
-  
+
+
   
   solar_map <- reactive({
     solar_data %>% ungroup() %>% group_by(State, month) %>% 
@@ -20,19 +23,46 @@ function(input, output){
       filter(month == as.numeric(input$month)) %>% arrange(monthly_irradiance)
   })
   
+  test_data <- reactive({
+    solar_data %>% mutate(panel_perf = efficiency*avg_ghi*input$area) %>% 
+      mutate(cons = multiplier*as.numeric(consumption[consumption$abb == input$state, 4])) %>%
+      filter(State == input$state & month == month(input$date) & day == mday(input$date))
+      
+  })
+  
   state_data <- reactive({
-    solar_data %>% ungroup() %>%filter(State == input$state  & month == input$month) %>%
+      temp<-solar_data %>% ungroup() %>% filter(State == input$state & month == input$month) %>%
       group_by(time) %>% summarise(hourly_ghi = mean(avg_ghi)) %>%
       mutate(panel_perf = efficiency*hourly_ghi*input$area) %>%
-      mutate(cons = multiplier*consumption[consumption$abb == input$state, 3])
+      mutate(cons = multiplier*as.numeric(consumption[consumption$abb == input$state, 4])) %>% 
+      ungroup() %>% mutate(total = sum(panel_perf))  %>%
+      mutate(index = as.numeric(substr(time, 1,2))) %>% 
+      mutate(charge = input$capacity*500)
+      
+      chargevec = c()
+      for (i in 1:24){
+        chargevec[i] = ifelse(i ==1, input$capacity*500,
+                           ifelse(chargevec[i-1]>input$capacity*1000,
+                                  input$capacity*1000,
+                                  chargevec[i-1]) + temp$panel_perf[i] -
+                             temp$cons[i])}
+
+      temp$charge = chargevec
+      
+      temp  #%>% mutate(charge = ifelse(charge >input$capacity*1000, input$capacity*1000, charge))
+      #mutate(battery = (input$capacity*500  + panel_perf  - index*cons)) %>%
+       #mutate(battery = ifelse(battery >input$capacity*1000, input$capacity*1000, battery))
+      
   }) 
+  
+  
   
  output$map <- renderGvis({
    gvisGeoChart(solar_map(), "State", 'monthly_irradiance',
                 options=list(region="US", displayMode="regions",
                              resolution="provinces",
                              colorAxis="{colors:['grey', 'orange']}",
-                             backgroundColor="lightblue",
+                             backgroundColor="#2f4f4f",
                              title = 'Average Monthly Irradiance Map',
                              width="auto", height="auto"))
  })
@@ -41,19 +71,24 @@ function(input, output){
  output$hist <- renderGvis(
    gvisHistogram(solar_map()[,'monthly_irradiance',
                            drop=FALSE],
-                 options = list(colors="['orange']")))
+                 options = list(colors="['orange']",
+                                backgroundColor="#2f4f4f")
+                 )
+   )
 
 
  output$daily <- renderGvis(
-   gvisColumnChart(state_data(), xvar = 'time' , yvar = c( 'panel_perf', 'cons'),
-                options = list(width = 500, height = 500,
-                               title = 'Hourly Energy Chart',
-                               legend="bottom",
-                               bar="{groupWidth:'100%'}"))
+   gvisColumnChart(state_data(), xvar = 'time' , yvar = c( 'panel_perf', 'cons', 'charge'),
+                   options = list(width = "100%", height = 400,
+                                  vAxes="[{title:'MYTEXT'}]",
+                                  title = 'Hourly Energy Chart',
+                                  legend="bottom",
+                                  bar="{groupWidth:'100%'}"))
                  
  )
-
  
+
+   
  
 }
 
--- ui.R
+++ ui.R
@@ -17,6 +17,7 @@ dashboardPage(
                 min = 1, 
                 max = 12, 
                 value = 6),
+
     selectInput('state', 
                 label = h3('Select State'),
                 choices = list(state.abb = state.abb), 
@@ -34,16 +35,32 @@ dashboardPage(
     tabItem(
       tabName = "map",
       fluidRow(box(htmlOutput("map"),
-                   title = h2('Mothly Insolation Map'))
+                   title = h2('Mothly Insolation Map'),
+                   width = 12)
                ),
-      fluidRow(box(htmlOutput("hist")))
+      fluidRow(box(htmlOutput("hist"),
+                   width = 12)
+               )
     ),
     tabItem(
       tabName = "daily",
       fluidRow(
-        box(infoBoxOutput("daily"))
+        box(htmlOutput("daily"),
+            width = 12)
+        ),
+      fluidRow(
+        box(sliderInput('capacity',
+                        label = h4('Select your battery capacity'),
+                        min = 1,
+                        max = 200,
+                        value = 30)),
+        box(dateInput("date", 
+                      label = h3("Date input"), 
+                      value = "2017-01-01"),
+            width = 2)
       )
-    )
+
   )
+)
 )
 )
